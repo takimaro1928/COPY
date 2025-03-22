22e24016f79f5aec623a1cd7a3431c13c086a5cc
takimaro1928 <ta11204649@gmail.com>
2025-03-22 21:28:07 +0900

    Create types.js

---
+++ src/models/types.js
@@ -0,0 +1,13 @@
+// src/models/types.js
+
+/**
+ * 理解度レベル
+ */
+export const UnderstandingLevel = {
+  FULL: 'full',     // 理解○
+  PARTIAL: 'partial', // 曖昧△
+  NONE: 'none'      // 理解できていない×
+};
+
+// その他の型定義はJavaScriptでは不要なので省略
+// TypeScriptの型情報はランタイムでは使用されない
