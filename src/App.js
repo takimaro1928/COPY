67422d79f99d0acf96ae28650b89bacf93b7a6e0
takimaro1928 <ta11204649@gmail.com>
2025-03-22 21:06:21 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+// src/App.js
+
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
@@ -6,13 +8,14 @@ import { AppProvider } from './contexts/AppContext';
 import Layout from './components/layout/Layout';
 import TodayProblems from './components/pages/TodayProblems/TodayProblems.js';
 import AllProblems from './components/pages/AllProblems/AllProblems.js';
+import SearchPage from './components/pages/Search/SearchPage.js';
 import ScheduleView from './components/pages/ScheduleView/ScheduleView.js';
 import Statistics from './components/pages/Statistics/Statistics.js';
 import VagueProblems from './components/pages/VagueProblems/VagueProblems.js';
+import DataManagement from './components/pages/DataManagement/DataManagement.js';
 import Settings from './components/pages/Settings/Settings.js';
 import NotFound from './components/pages/NotFound/NotFound.js';
-import DataManagement from './components/pages/DataManagement/DataManagement.js';
-import SearchPage from './components/pages/Search/SearchPage.js';
+import InitialSetup from './components/pages/InitialSetup/InitialSetup.js';
 
 // テーマの設定
 const theme = createTheme({
@@ -75,14 +78,15 @@ const App = () => {
           <Routes>
             <Route path="/today" element={<TodayProblems />} />
             <Route path="/all-problems" element={<AllProblems />} />
+            <Route path="/search" element={<SearchPage />} />
             <Route path="/schedule" element={<ScheduleView />} />
             <Route path="/statistics" element={<Statistics />} />
             <Route path="/vague-problems" element={<VagueProblems />} />
+            <Route path="/data-management" element={<DataManagement />} />
             <Route path="/settings" element={<Settings />} />
+            <Route path="/initial-setup" element={<InitialSetup />} />
             <Route path="/" element={<Navigate to="/today" replace />} />
             <Route path="*" element={<NotFound />} />
-  　　　　　　<Route path="/data-management" element={<DataManagement />} />
-  　　　　　　<Route path="/search" element={<SearchPage />} />
           </Routes>
         </Layout>
       </AppProvider>
